1e54789627eb6065af8c5482c7bc7faab8d49709
Gireesh Ramji <gireesh@fractal-data.com>
2023-01-31 22:11:13 +1100

    2.1.1: Data generation function and tests implemented - next step, visualize generated data in streamlit

---
+++ data_generation/interplanetary_distance.py
@@ -0,0 +1,33 @@
+import math
+
+def orbital_period_2_angular_velocity(orbital_period_days):
+    return 2*math.pi/orbital_period_days
+
+def get_interplanetary_distance(orbital_radius_km_planet_1,
+                                orbital_radius_km_planet_2,
+                                orbital_period_days_planet_1,
+                                orbital_period_days_planet_2,
+                                time_since_start):
+    orbital_radius_ratio = orbital_radius_km_planet_2/orbital_radius_km_planet_1
+    angular_velocity_rad_planet_1 = orbital_period_2_angular_velocity(orbital_period_days_planet_1)
+    angular_velocity_rad_planet_2 = orbital_period_2_angular_velocity(orbital_period_days_planet_2)
+    squared_distance = (orbital_radius_km_planet_1 ** 2) * \
+                       (1 + ((orbital_radius_ratio)**2)
+                        - 2*(orbital_radius_ratio)
+                        *(math.cos((angular_velocity_rad_planet_2 - angular_velocity_rad_planet_1)*time_since_start)))
+    interplanetary_distance = squared_distance**0.5
+    return interplanetary_distance
+
+def generate_distances_for_years(num_years,
+                                 orbital_radius_km_planet_1,
+                                 orbital_radius_km_planet_2,
+                                 orbital_period_days_planet_1,
+                                 orbital_period_days_planet_2):
+    num_days = num_years*365 + int(num_years/4)
+    t = range(num_days)
+    distances = [get_interplanetary_distance(orbital_radius_km_planet_1,
+                                             orbital_radius_km_planet_2,
+                                             orbital_period_days_planet_1,
+                                             orbital_period_days_planet_2,t_x)
+                 for t_x in t]
+    return distances
